79d89f4abd20e0e599f61e2351b05f3e665a5441
jn484 <jn484@Cornell.edu>
2019-10-29 20:08:01 -0400

    modified ship

--- ship.ml
+++ ship.ml
@@ -1,18 +1,25 @@
-type order = LT | GT | EQ
-
-module type Comparable = sig
-  type t
-  val compare: t -> t -> order
-end
-
-module type Formattable = sig
-  type t
-  val format: Format.formatter -> t -> unit
-end
-
 module type Ship = sig
   type t
-  val yes: int
+  val empty : t
+  val create : t
+  val hit : string -> t -> unit
+  val alive : t -> bool
 end
 
-module ShipMaker
+module ShipMaker = struct
+  type t = (string * bool) list
+
+  let empty = []
+
+  let create lst =
+    List.map (fun x -> (x, true)) lst
+
+  let rec hit coord = function
+    | [] -> []
+    | (s, b)::t -> if s = coord then (s, false)::t else (s, b)::hit coord t
+
+  let rec alive = function
+    | [] -> false
+    | (s, b)::t -> if b then true else alive t
+
+end
