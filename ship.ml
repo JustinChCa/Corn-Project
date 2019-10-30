a490050e3aab3194731ac1a4d4ccd3022eede499
jn484 <jn484@Cornell.edu>
2019-10-29 20:14:24 -0400

    s

--- ship.ml
+++ ship.ml
@@ -6,12 +6,14 @@ module type Ship = sig
   val alive : t -> bool
 end
 
+type coor = string
+
 module ShipMaker = struct
-  type t = (string * bool) list
+  type t = (coor * bool) list
 
   let empty = []
 
-  let create lst =
+  let create (lst:coor list) =
     List.map (fun x -> (x, true)) lst
 
   let rec hit coord = function
@@ -21,5 +23,6 @@ module ShipMaker = struct
   let rec alive = function
     | [] -> false
     | (s, b)::t -> if b then true else alive t
-
 end
+
+let bigShip = ShipMaker.create ["A1"; "B1"]
