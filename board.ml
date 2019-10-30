75ca0d1e4f773bb552daa1bbf7f6761d35c90809
Jerry Zhou <hz343@cornell.edu>
2019-10-29 20:28:33 -0400

    Implemented board.ml at inital stage
    - defined type opt for board
    - defined type t for board
    - created fun hit
    - created fun columns
    - created fun rows
    - added documentation for type t and fun rows and columns

--- board.ml
+++ board.ml
@@ -1,3 +1,45 @@
+
+open ShipMaker
+
 module Board = struct
-  Array.make
+
+  type opt = Miss | Hit | Water of ShipMaker.t option
+
+
+  (**AF: The array [| r1; r2; r3; r4;... |] is the array of the row arrays
+     ri = [|ai1; ai2; ai3; ai4; ... |] which represents the elements in left to 
+     right order of the elements in that row. aij is the element in the ith row
+     and the jth column of a matrix of dimensions ixj.
+     RI: All rows are the same size and all columns are the same size. Every
+     element of the array must contain an element of type opt.*)
+  type t = (opt array) array 
+
+
+  let hit (b:t) (pair:int*int) : unit = 
+    match pair with
+    |(r, c) -> begin
+        match (b.(r)).(c) with 
+        | Miss
+        | Hit -> print_endline "You have already attacked here"
+        (*TODO: go to the next player's turn *)
+        | Water op -> begin
+            match op with 
+            | None -> (b.(r)).(c) <- Miss
+            | Some ship -> (b.(r)).(c) <- Hit
+          end
+      end
+
+  let dis_board b = 
+    failwith "unimplemented"
+
+  (** [columns b] gives the number of columns in the board [b]. This is equal to
+      the size of each row in [b] *)
+  let columns b = Array.length b.(1)
+
+
+  (** [rows b] gives the number of rows in the board [b]. This is equal to the
+      size of each column in [b]  *)
+  let rows b = Array.length b
+
+
 end
