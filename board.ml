cc8f9381327199a5a50c22578ffaaa1ccb725069
Jerry Zhou <hz343@cornell.edu>
2019-11-01 19:20:33 -0400

    added documentation for board

--- board.ml
+++ board.ml
@@ -5,13 +5,6 @@ module Board = struct
 
   type opt = Miss | Hit | Water of ShipMaker.t option
 
-  let opt_to_str x = 
-    match x with
-    |Miss -> print_string "|x"
-    |Hit -> print_string "|o"
-    |Water None -> print_string "| "
-    |Water Some _ -> print_string "|s"
-
   (**AF: The array [| r1; r2; r3; r4;... |] is the array of the row arrays
      ri = [|ai1; ai2; ai3; ai4; ... |] which represents the elements in left to 
      right order of the elements in that row. aij is the element in the ith row
@@ -20,7 +13,8 @@ module Board = struct
      element of the array must contain an element of type opt.*)
   type t = (opt array) array 
 
-
+  (** [hit b pair] augments the board [b] based on an attack on the
+      coordinates [pair]. *)
   let hit (b:t) (pair:int*int) : unit = 
     match pair with
     |(r, c) -> begin
@@ -35,10 +29,13 @@ module Board = struct
           end
       end
 
+  (** [h_partition str n] creates the horizontal partition needed for the 
+      console command graphic. *)
   let rec h_partition str n = 
     if n > 0 then h_partition (str^"==") (n-1) else str
 
-
+  (** [opt_to_str x] converts an [opt] to its corresponding string for the 
+      console command graphic.*)
   let opt_to_str x = 
     match x with
     |Miss -> print_string "|x"
@@ -46,14 +43,14 @@ module Board = struct
     |Water None -> print_string "| "
     |Water Some _ -> print_string "|s"
 
-
+  (** [dis_row str r] displays the console command graphic of a row [r].*)
   let dis_row str r =
     Array.iter opt_to_str r;
     print_endline "|";
     print_endline str;;
 
-
-  let dis_board fmt b = 
+  (** [dis_board b] gives a console command graphic of the board.*)
+  let dis_board b = 
     let partition = h_partition "" (Array.length b.(1)) in
     print_string partition;
     Array.iter (dis_row partition) b;;
