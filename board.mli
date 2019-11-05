8d072f634a03acfa7f987f0b46f316cdcd0451f0
Jerry Zhou <hz343@cornell.edu>
2019-11-04 20:28:51 -0500

    merge

--- board.mli
+++ board.mli
@@ -6,35 +6,46 @@ module type Board = sig
   type opt = Miss | Hit | Water of ShipMaker.t option
 
   exception Overlap
-  exception Out_of_Bounds
+  exception OutOfBounds
 
-  (** the type implementation of the board, an opt array array. *)
+  (**AF: The array [| r1; r2; r3; r4;... |] is the array of the row arrays
+     ri = [|ai1; ai2; ai3; ai4; ... |] which represents the elements in left to 
+     right order of the elements in that row. aij is the element in the ith row
+     and the jth column of a matrix of dimensions ixj.
+     RI: All rows are the same size and all columns are the same size. Every
+     element of the array must contain an element of type opt.*)
   type t 
 
+  (** [make_board x y] makes a matrix with [x] rows and [y] columns with every 
+      element in the matrix being [Water None]. *)
   val make_board: int -> int -> t
 
-  (** [hit b pair] changes the board [b] after a coordinate 
-      [pair] is attacked. *)
+  (** [hit b pair] augments the board [b] based on an attack on the
+      coordinates [pair]. Returns a message that the player has already
+      attacked an area if they input that coordinate; the player will not get 
+      another turn if they do. *)
   val hit: t -> int*int -> unit
 
   (** [dis_board b] displays the board in graphical form in the console command
       window. *)
   val dis_board: t -> unit
 
-  (** [columns b] gives the number of columns in board [b] *)
+  (** [columns b] gives the number of columns in the board [b]. This is equal to
+      the size of each row in [b] *)
   val columns: t -> int
 
-  (** [rows b] give the number of rows in board [b] *)
+  (** [rows b] gives the number of rows in the board [b]. This is equal to the
+      size of each column in [b]  *)
   val rows: t -> int
 
   (** [place_ship_h b ship] puts ship horizontally into the board [b].
-      Raises: Out_of_Bounds if the ship exceeds the bounds of the board.
+      Raises: OutOfBounds if the ship exceeds the bounds of the board.
           Overlap if the ship will overlap with an existing ship on the board.*)
   val place_ship_h: t -> ShipMaker.t -> unit
 
 
   (** [place_ship_h b ship] puts ship vertically into the board [b].
-      Raises: Out_of_Bounds if the ship exceeds the bounds of the board.
+      Raises: OutOfBounds if the ship exceeds the bounds of the board.
           Overlap if the ship will overlap with an existing ship on the board.*)
   val place_ship_v: t -> ShipMaker.t -> unit
 
--- test.ml
+++ test.ml
@@ -1,21 +1,32 @@
 open OUnit2
-open Ship
-open Board
 open Commands
-open Player
 
-module Player = PlayerMaker
 
-module Board = BoardMaker
+module Player = Player.PlayerMaker
 
-module Ship = ShipMaker
+module Board = Board.BoardMaker
+
+module Ship = Ship.ShipMaker
 
 let board = Board.make_board 10 10
 let ship_destroyer = Ship.create [(1,1);(1,2);(1,3)]
 let ship_sub = Ship.create [(2,1);(3,1)]
+let player = Player.init_player [ship_destroyer; ship_sub] board "player 1"
+
+open Ship
+let ship_tests = [
+
+  "is_empty against empty" >:: (fun _ -> assert_equal true (is_empty empty));
+  "is empty against nonempty" >:: 
+  (fun _ -> assert_equal false (is_empty ship_sub));
+
+  "size with empty is 0" >:: (fun _ -> assert_equal 0 (size empty));
+  "size with destroyer is 3" >:: (fun _ -> assert_equal 3 (size ship_destroyer));
+  "size with sub is 2" >:: (fun _ -> assert_equal 2 (size ship_sub));
 
 
-let ship_tests = []
+
+]
 
 let board_tests = []
 
