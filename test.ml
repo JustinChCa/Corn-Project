7c01638ad87261cab044a72e6da9ae01c0647e36
Jerry Zhou <hz343@cornell.edu>
2019-11-05 18:43:15 -0500

    working on test cases

--- test.ml
+++ test.ml
@@ -17,12 +17,14 @@ let player = Player.init_player [ship_destroyer; ship_sub] board "player 1"
 open Ship
 let ship_tests = [
 
-  "is_empty against empty" >:: (fun _ -> assert_equal true (is_empty empty));
+  "is_empty against empty" >:: 
+  (fun _ -> assert_equal true (is_empty (empty ()) ));
   "is empty against nonempty" >:: 
   (fun _ -> assert_equal false (is_empty ship_sub));
 
-  "size with empty is 0" >:: (fun _ -> assert_equal 0 (size empty));
-  "size with destroyer is 3" >:: (fun _ -> assert_equal 3 (size ship_destroyer));
+  "size with empty is 0" >:: (fun _ -> assert_equal 0 (size (empty ())));
+  "size with destroyer is 3" >:: 
+  (fun _ -> assert_equal 3 (size ship_destroyer));
   "size with sub is 2" >:: (fun _ -> assert_equal 2 (size ship_sub));
 
 
